5ddba35aab0f2646f1beb322d096f8fb8b829ceb
Ravindra Valand <ravindra@linearloop.io>
2024-09-03 16:07:19 +0530

    stash

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
-import { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg'; // Changed from '/vite.svg' to './vite.svg' to correct the path
+import viteLogo from './vite.svg';
 import './App.css';
 import Header from './header/head';
 
-class App extends React.Component {
-  constructor(props) {
-    // Intentionally breaking the rule by not calling super(props)
+interface AppState {
+  count: number;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
     this.state = { count: 0 };
   }
 
   render() {
-    // Breaking SonarQube rule: Cognitive Complexity of functions should not be too high
-    function complexFunction(n: number): number {
-      if (n <= 1) return n;
-      if (n % 2 === 0) {
-        return complexFunction(n / 2);
-      } else {
-        return complexFunction(3 * n + 1);
-      }
-    }
-
-    // Breaking SonarQube rule: "any" type should not be used
-    let unsafeVariable: any = "This could be anything";
+    const incrementCount = () => {
+      this.setState(prevState => ({ count: prevState.count + 1 }));
+    };
 
     return (
       <Router>
@@ -41,14 +36,12 @@ class App extends React.Component {
               </div>
               <h1>Vite + React</h1>
               <div className="card">
-                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+                <button onClick={incrementCount}>
                   count is {this.state.count}
                 </button>
                 <p>
                   Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
-                {/* Breaking SonarQube rule: React components should not have too many props */}
-                <TooManyProps prop1={1} prop2={2} prop3={3} prop4={4} prop5={5} prop6={6} prop7={7} prop8={8} prop9={9} prop10={10} prop11={11} prop12={12} />
               </div>
               <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
@@ -64,9 +57,5 @@ class App extends React.Component {
   }
 }
 
-// Breaking SonarQube rule: Functions should not be too complex
-function TooManyProps(props: any) {
-  return <div>{Object.values(props).join(', ')}</div>;
-}
 
 export default App;
